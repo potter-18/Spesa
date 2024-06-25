e5d15c615f83597db144b1865b356d1aa500e022
potter-18 <ankityadaav13@gmail.com>
2024-06-25 18:45:53 +0530

    Add Transaction againgst which refund is issued.

--- Spesa/Item.swift
+++ Spesa/Item.swift
@@ -11,19 +11,24 @@ import SwiftData
 enum TransactionMode: Int, Codable {
     case Expense
     case Income
+    case Refund
 }
 
 @Model
-final class Item {
+final class Item: Identifiable {
+    var id = UUID()
     var title: String
     var amount: Double
     var transactionMode: TransactionMode
     var transactionOption: String
+    var RefundAgainst: UUID?
     
-    init(title: String, amount: Double, transactionMode: TransactionMode, transactionOption: String) {
+    init(id: UUID = UUID(), title: String, amount: Double, transactionMode: TransactionMode, transactionOption: String, RefundAgainst: UUID? = nil) {
+        self.id = id
         self.title = title
         self.amount = amount
         self.transactionMode = transactionMode
         self.transactionOption = transactionOption
+        self.RefundAgainst = RefundAgainst
     }
 }
